2870249165ac47eec9d310cef20f14ecd6d46eb9
Jacob Gosmer <eaajago@students.dk>
2023-06-09 11:28:00 +0200

    sortering Til arrayList

---
+++ Sortering/src/sortering/InsertionSortering.java
@@ -0,0 +1,40 @@
+package sortering;
+
+import java.util.ArrayList;
+
+public class InsertionSortering {
+
+    public static void insertioneSort(int[] list) {
+        for (int i = 1; i < list.length; i++) {
+            int next = list[i];
+            int j = i;
+            boolean found = false;
+            while (!found && j > 0) {
+                if (next >= list[j - 1]) {
+                    found = true;
+                } else {
+                    list[j] = list[j - 1];
+                    j--;
+                }
+            }
+            list[j] = next;
+        }
+    }
+
+    public static void insertionSortArrayList(ArrayList<Integer> list) {
+        for (int i = 1; i < list.size(); i++) {
+            int next = list.get(i);
+            int j = i;
+            boolean found = false;
+            while (!found && j > 0) {
+                if (next >= list.get(j - 1)) {
+                    found = true;
+                } else {
+                    list.set(j, list.get(j - 1));
+                    j--;
+                }
+            }
+            list.set(j, next);
+        }
+    }
+}
