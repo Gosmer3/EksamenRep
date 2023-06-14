1af6474d1fb40b7703e42fd4c2968d115516c000
LasseLiin <eaalalk@students.eaaa.dk>
2023-06-14 12:07:24 +0200

    Lavet flere opgaver i rekursion

--- Rekursion/src/OpgaverRekursion1/Opgave1.java
+++ Rekursion/src/OpgaverRekursion1/Opgave1.java
@@ -4,23 +4,23 @@ public class Opgave1 {
 
 
     //opgave 1
-    public static int factorial(int n){
+    public static int factorial(int n) {
         int resultat = 0;
-        if(n <= 1){
-            resultat =  n;
-        }else{
-            resultat = n * factorial(n-1);
+        if (n <= 1) {
+            resultat = n;
+        } else {
+            resultat = n * factorial(n - 1);
         }
-        return  resultat;
+        return resultat;
     }
 
     //Opgave 2
-    public static int power(int n, int p){
+    public static int power(int n, int p) {
         int result = 0;
-        if(p == 1){
+        if (p == 1) {
             result = 1;
-        }else{
-            result = power(n,p-1) *n;
+        } else {
+            result = power(n, p - 1) * n;
         }
         return result;
     }
@@ -33,6 +33,7 @@ public class Opgave1 {
             return product(a - 1, b) + b;
         }
     }
+
     public static int productRus(int a, int b) {
         if (a == 0) {
             return 0;
@@ -43,8 +44,34 @@ public class Opgave1 {
         }
     }
 
-    //Opgave 4
-//    public static String reverse(String s){
-//
-//    }
+    //Opgave 4 - omvend bogstavsrækkefølgen
+    public static String reverse(String s) {
+        if (s.isEmpty()) {
+            return "";
+        }
+        char last = s.charAt(s.length() - 1);
+        String remainingWord = s.substring(0, s.length() - 1);
+        return last + reverse(remainingWord);
+    }
+
+    //Opgave 5 - Euclids algoritme
+    public static int SFD(int a, int b) {
+        if (b <= a && (a % b == 0)) {
+            return b;
+        }
+        if (a < b) {
+            return SFD(b, a);
+        }
+        return SFD(b, a%b);
+    }
+
+    public static int domino(int n){
+if(n <= 0){
+    return 0;
+}if(n == 1){
+    return 1;
+        }else{
+    return domino(n-2) + domino(n-1);
+        }
+    }
 }
---
+++ Rekursion/src/OpgaverRekursion1/Opgave1App.java
@@ -0,0 +1,19 @@
+package OpgaverRekursion1;
+
+import java.sql.SQLOutput;
+
+public class Opgave1App {
+    public static void main(String[] args) {
+
+
+//opgave reverse
+        System.out.println(Opgave1.reverse("jeH"));
+
+//opgave 5 euclids algoritme
+        System.out.println(Opgave1.SFD(40, 100));
+
+//opgave 6 domino
+        System.out.println(Opgave1.domino(20));
+    }
+
+}
