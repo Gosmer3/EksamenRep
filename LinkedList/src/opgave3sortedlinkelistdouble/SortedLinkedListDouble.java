944cd3a36bb4087350f2ccaeaf72ba5f9e57f83f
Jacob Gosmer <eaajago@students.dk>
2023-06-20 11:59:54 +0200

    ??

--- LinkedList/src/opgave3sortedlinkelistdouble/SortedLinkedListDouble.java
+++ LinkedList/src/opgave3sortedlinkelistdouble/SortedLinkedListDouble.java
@@ -2,63 +2,98 @@ package opgave3sortedlinkelistdouble;
 
 public class SortedLinkedListDouble {
 
-	private Node first;
-	private Node last;
-	private int size;
+    private Node first;
+    private Node last;
+    private int size;
 
-	public SortedLinkedListDouble() {
-		first = new Node();
-		last = new Node();
-		first.next = last;
-		last.prev = first;
-		size = 0;
-	}
+    public SortedLinkedListDouble() {
+        first = new Node();
+        last = new Node();
+        first.next = last;
+        last.prev = first;
+        size = 0;
+    }
 
-	/**
-	 * Tilføjer e til listen, så listen fortsat er sorteret i henhold til den
-	 * naturlige ordning på elementerne
-	 */
-	public void addElement(String e) {
-		// TODO
-	}
+    /**
+     * Tilføjer e til listen, så listen fortsat er sorteret i henhold til den
+     * naturlige ordning på elementerne
+     */
+    public void addElement(String e) {
+        Node newNode = new Node();
+        newNode.data = e;
 
-	/**
-	 * Fjerner den første forekomst af e i listen. Listen skal fortsat være
-	 * sorteret i henhold til den naturlige ordning på elementerne.
-	 *
-	 * @return true hvis e blev fjernet fra listen ellers returneres false.
-	 */
-	public boolean removeElement(String e) {
-	// TODO
-		return false;
-	}
+        Node current = first.next;
+        while (current != last && e.compareTo(current.data) > 0) {
+            current = current.next;
+        }
 
-	/**
-	 * Udskriver elementerne fra listen i sorteret rækkefølge 
-	 */
-	public void udskrivElements() {
-		//TODO
-	}
+        Node previous = current.prev;
+        previous.next = newNode;
+        newNode.prev = previous;
+        newNode.next = current;
+        current.prev = newNode;
 
-	/**
-	 * Udskriver elementerne fra listen i sorteret rækkefølge bagfra
-	 */
-	public void udskrivBagfra() {
-		// TODO
-	}
+        size++;
+    }
 
-	/**
-	 * Returnerer antallet af elementer i listen
-	 */
-	public int countElements() {
-		// TODO
-		return -1;
-	}
+    /**
+     * Fjerner den første forekomst af e i listen. Listen skal fortsat være
+     * sorteret i henhold til den naturlige ordning på elementerne.
+     *
+     * @return true hvis e blev fjernet fra listen ellers returneres false.
+     */
+    public boolean removeElement(String e) {
+        Node current = first.next;
+        while (current != last) {
+            if (current.data.equals(e)) {
+                Node previous = current.prev;
+                Node next = current.next;
+                previous.next = next;
+                next.prev = previous;
+                size--;
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
 
-	// Privat indre klasse der modellerer en node i en dobbeltkædet liste
-	class Node {
-		public String data;
-		public Node next;
-		public Node prev;
-	}
+    /**
+     * Udskriver elementerne fra listen i sorteret rækkefølge
+     */
+    public void udskrivElements() {
+		Node temp = first;
+
+		while (temp != null){
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+    }
+
+    /**
+     * Udskriver elementerne fra listen i sorteret rækkefølge bagfra
+     */
+    public void udskrivBagfra() {
+
+		Node temp = last;
+
+		while (temp != first){
+			System.out.println(temp.data);
+			temp = last.prev;
+		}
+    }
+
+    /**
+     * Returnerer antallet af elementer i listen
+     */
+    public int countElements() {
+		return size;
+    }
+
+    // Privat indre klasse der modellerer en node i en dobbeltkædet liste
+    class Node {
+        public String data;
+        public Node next;
+        public Node prev;
+    }
 }
